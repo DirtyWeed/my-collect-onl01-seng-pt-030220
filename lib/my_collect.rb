85c15d4123001da9b62144fac9220b6cb253b292
Dylan Williamson <dirtyassweed@icloud.com>
2020-03-14 15:31:45 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,30 @@
 def my_collect(collection)
   i = 0
-  new_collection = []
+  new = []
   while i < collection.length
-    new_collection << yield(collection[i])
-    i += 1
+    new << yield(collection[i])
   end
-  new_collection
+  new
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+#def my_collect(collection)
+#  i = 0
+#  new_collection = []
+#  while i < collection.length
+#    new_collection << yield(collection[i])
+#    i += 1
+#  end
+#  new_collection
+#end
